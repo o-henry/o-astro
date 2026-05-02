d3e521a2698d264bc0eeb315515c2759a351a2e9
Henry <c.henry.9209@gmail.com>
2026-05-02 12:35:43 +0900

    Hide which-key mapping icons

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,9 @@ return {
   {
     "folke/which-key.nvim",
     opts = function(_, opts)
+      opts.icons = opts.icons or {}
+      opts.icons.mappings = false
+
       opts.replace = opts.replace or {}
       opts.replace.desc = opts.replace.desc or {}
       table.insert(opts.replace.desc, function(desc) return desc:upper() end)
